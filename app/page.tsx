e13c78e9f0ae1269f7b5a0e8c2dbc695ca0d5118
RoumesaShabbir45 <roumesa45@gmail.com>
2026-08-30 07:46:54 -0700

    satic rendering(build time)

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,8 @@
-import { json } from "node:stream/consumers";
-
-interface Post {
-    id: number;
-    title: string;
-    body: string;
-}
-
-export default async function home() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts',
-        { cache:"no-store"
-    });
-    if (!res.ok){
-        throw new Error('failed to fecth posts');
-    }
-
-    const posts: Post[] = await res.json();
-
-    return (
+export default function Home(){
+    return(
         <div>
-            <h1>posts fetch data by server</h1>
-            <ul>
-                {posts.slice(0, 5).map(post =>
-                    <li key={post.id}>
-                        <h2>{post.title}</h2>
-                        <p>{post.body}</p>
-                    </li>
-                )}
-            </ul>
+            <h1>Welcome to home page(Static Render Page)</h1>
+            <p>This is main rending page of application.</p>
         </div>
     );
-
 }
