10001c2061426152d8cb817174a92d6017c2e412
RoumesaShabbir45 <roumesa45@gmail.com>
2026-08-30 04:28:00 -0700

    calling api on server component

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,18 @@
-import Image from 'next/image';
-// app/page.jsx
-export  default function Home( ){
+export  default  async function Home( ){
+
+    const data =await fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json());
     return(
-        <main style={{padding:'40px'}}>
-            <p>This is the main component</p>
-    <Image src='/eyes.jpg'
-     alt='EyeImage'
-     width={300}
-     height={200}
-     />
-     <hr></hr>
- <Image 
-  src="https://images.unsplash.com/photo-1501004318641-b39e6451bec6" 
-  alt='Flower'
-  width={200} 
-  height={200} 
-/>
-    </main>
+       <div>
+        <h1>Server Comp</h1>
+      <ul>
+        {data.map(item => (
+            <li key ={item.id}>
+                <h2>{item.title}</h2>
+                <p>{item.body}</p>
+            </li> 
+        ))}
+      </ul>
+       </div>
     );
 }
