0aa5c2f94d4fb977cdf38c5839cd7866114ccbac
RoumesaShabbir45 <roumesa45@gmail.com>
2026-08-30 07:59:30 -0700

    dynamic render pagr using json link

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-export default function Home(){
+export default async function Home(){
+    const res= await fetch('https://jsonplaceholder.typicode.com/posts',{
+        cache:'no-store'
+    })
+    const data = await res.json()
+    console.log(data)
     return(
         <div>
-            <h1>Welcome to home page(Static Render Page)</h1>
-            <p>This is main rending page of application.</p>
+            <h1>Welcome to home page(Dynamic Render Page)</h1>
+            <pre>{JSON.stringify(data,null,2)}</pre>
+            
         </div>
     );
-}
+} 
