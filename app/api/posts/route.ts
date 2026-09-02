b015c878251c7e6653448bc8256dc50651e8cfdc
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-02 07:27:25 -0700

    api routing post api

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,11 +1,22 @@
 import {NextResponse}from"next/server";
 
 const posts = [
-    {id:1,title:"learn Next.js"},
-    {id:2,title:"learn react"},
-    {id:3,title:"learn javascript"},
+    {id:1,title:"learn Next.js",content:"Next.js is a React framework for production"},
+    {id:2,title:"learn react",content:"React is a JavaScript library for building user interfaces"},
+    {id:3,title:"learn javascript",content:"JavaScript is a programming language that conforms to the ECMAScript specification"},
 ];
 
 export async function GET() {
     return NextResponse.json(posts);
-};
+}
+
+//post api
+export async function POST(request: Request) {
+    const body = await request.json();
+    return NextResponse.json({
+        message: "Post created successfully",
+        post: body,
+},{
+ status: 201,
+});
+}
