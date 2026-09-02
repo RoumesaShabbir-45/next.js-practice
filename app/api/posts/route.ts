f8534c8b425cb6787235cebdc8dbd893682ca648
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-02 08:23:44 -0700

    add new post and push new post

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,6 +1,6 @@
 import {NextResponse}from"next/server";
 
-const posts = [
+let posts = [
     {id:1,title:"learn Next.js",content:"Next.js is a React framework for production"},
     {id:2,title:"learn react",content:"React is a JavaScript library for building user interfaces"},
     {id:3,title:"learn javascript",content:"JavaScript is a programming language that conforms to the ECMAScript specification"},
@@ -13,6 +13,13 @@ export async function GET() {
 //post api
 export async function POST(request: Request) {
     const body = await request.json();
+     
+    const newPost = {
+        id: posts.length + 1,
+        title: body.title,
+        content: body.content
+    }
+    posts.push(newPost);
     return NextResponse.json({
         message: "Post created successfully",
         post: body,
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,68 @@
-import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
+"use client";
 
-export default async function HomePage(){
-    const response = await fetch("http://localhost:3000/api/posts");
-    const posts = await response.json();
-  return(
-        <div>
-            <h1>Posts from Get API</h1>
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
-                ))}
-            </ul>
+import { useEffect, useState } from "react";
+ 
+type Post = {
+    id: number;
+    title: string;
+    content: string;
+};
+export default function HomePage() {
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    async function getPosts() {
+        const response = await fetch("/api/posts");
+        const data = await response.json();
+        setPosts(data);
+    }
+
+    async function createPost() {
+        const response = await fetch("/api/posts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ title, content }),
+        });
+         setTitle("");
+        setContent("");
+        getPosts();
+    }
+      useEffect(() => {
+        getPosts();
+    }, []);
+     
+    return (
+        <div style={{ padding:20}}>
+            <h1>GET AND POST API</h1>
+            <button onClick={getPosts}> GET POSTS</button>
+
+             <div style={{ marginTop:20 }}></div>
+             <input
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)} 
+                />
+             <br />
+             <input
+                placeholder="Content"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                 />
+             <br />
+             <div style ={{ marginTop:10 }}>
+                <button onClick={createPost}>CREATE POST</button>
+             </div>
+             <pre> {JSON.stringify(posts,null,2)}</pre>
         </div>
     );
-} 
+}
+
+
+            
+
+
+
+
