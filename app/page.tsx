6eafae727a7921fa2c12407cc7aeddceda23b874
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-03 08:30:20 -0700

    implemention of post get and patch api

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
- 
+
 type Post = {
     id: number;
     title: string;
@@ -11,6 +11,7 @@ export default function HomePage() {
     const [posts, setPosts] = useState<Post[]>([]);
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
+    const [id, setId] = useState("");
 
     async function getPosts() {
         const response = await fetch("/api/posts");
@@ -26,19 +27,20 @@ export default function HomePage() {
             },
             body: JSON.stringify({ title, content }),
         });
-         setTitle("");
+        setTitle("");
         setContent("");
         getPosts();
     }
 
     async function patchPost() {
-        const response = await fetch(`/api/posts/${title}`, {
+        const response = await fetch(`/api/posts/${id}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ title, content }),
+            body: JSON.stringify({ title }),
         });
+        setId("");
         setTitle("");
         setContent("");
         getPosts();
@@ -47,44 +49,44 @@ export default function HomePage() {
     useEffect(() => {
         getPosts();
     }, []);
-     
+
     return (
-        <div style={{ padding:20}}>
+        <div style={{ padding: 20 }}>
             <h1>GET PATCH AND POST API</h1>
             <button onClick={getPosts}> GET POSTS</button>
 
-            <input 
-            placeholder="id (ForPATCH)"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            <br />
+            <div style={{ marginTop: 20 }}></div>
+            <input
+                placeholder="id (For PATCH)"
+                value={id}
+                onChange={(e) => setId(e.target.value)}
             />
-
-
-             <div style={{ marginTop:20 }}></div>
-             <input
+            <br />
+            <input
                 placeholder="Title"
                 value={title}
-                onChange={(e) => setTitle(e.target.value)} 
-                />
-             <br />
-             <input
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <br />
+            <input
                 placeholder="Content"
                 value={content}
                 onChange={(e) => setContent(e.target.value)}
-                 />
-             <br />
-             <div style ={{ marginTop:10 }}>
-                <button onClick={createPost}>CREATE POST</button>
-                
+            />
+            <br />
+            <div style={{ marginTop: 10 }}>
+                <button onClick={createPost}>POST</button>
                 <button onClick={patchPost}>PATCH</button>
-             </div>
-             <pre> {JSON.stringify(posts,null,2)}</pre>
+            </div>
+
+            <pre> {JSON.stringify(posts, null, 2)}</pre>
         </div>
     );
 }
 
 
-            
+
 
 
 
