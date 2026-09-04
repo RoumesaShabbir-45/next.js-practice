b813e450a75a24b7ec609167ccf5f51cf48b70d7
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-04 07:44:11 -0700

    get api in login

---
+++ app/api/login/Profile/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { verifyToken } from "@/lib/jwt";
+import { error } from "console";
+
+export async function GET(request:Request) 
+{
+    const authHeader = request.headers.get("Authorization");
+
+    if(!authHeader)
+    {
+        return NextResponse.json(
+
+            { message : "unauthorize"},
+            {status:401}
+        );
+    }
+    const token = authHeader.split("")[1];
+    
+    try{
+        const user = verifyToken(token);
+        return NextResponse.json(
+            {
+                message : "protected data",
+                user,
+            });
+    }
+    catch(error) {
+        return NextResponse.json(
+           { message :"invalid token"},
+           {status:401}
+        );
+    }
+}
