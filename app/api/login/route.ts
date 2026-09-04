b7e8f8fe3e63f408a2592b5d3cd6d7c0285f4a43
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-04 07:24:07 -0700

    post api login

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,25 +1,22 @@
-import {NextResponse} from "next/server";
-export async function POST(request: Request){
-    const {email, password} = await request.json();
+import { NextResponse } from "next/server";
+import{ generateToken} from "@/lib/jwt"
 
+//post api
+export async function POST(request:Request) 
+{
+    const {email,password}= await request.json();
+    
     //demo credentials
-if (email === "admin@company.com" && password === "123456") {
-
-    const response = NextResponse.json({
+if (email === "admin@company.com" && password === "123456")
+{
+    const token = generateToken({email});
+    
+    return NextResponse.json({
          message: "Login successful" 
         });
-
-        //set auth cookie
-        response.cookies.set("auth", "true", { 
-            httpOnly: true, 
-            path: "/"
-         });
-
-        return response;
-    } 
+       
         return NextResponse.json(
             { message: "Invalid credentials" },
              { status: 401 });
-    }
-   
-     
+    };
+}
