5959c53bf0def1437c980951f1f35009173a7c01
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-04 05:38:27 -0700

    Fix middleware and login API routes

---
+++ app/login/page.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const router = useRouter();
+
+     async function handleLogin() {
+        const response = await fetch("/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email, password })
+        });
+
+        if (response.ok) {
+            router.push("/dashboard");
+        }
+        else {
+            alert("Login failed");
+    }
+    }
+
+    return (
+        <div style={{ padding: 20 }}>
+            <h1>Login</h1>
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleLogin}>Login</button>
+        </div>
+    );
+}
