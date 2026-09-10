7ab500fc0679ed95dd346bbfb721916872683257
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-10 07:24:00 -0700

    deleting data through id backend code

--- app/api/users/[id]/route.js
+++ app/api/users/[id]/route.js
@@ -1,25 +1,46 @@
-import {connectDB} from "@/lib/mongoose";
-import {NextResponse} from "next/server";
-import  User from "@/models/User";
+import { connectDB } from "@/lib/mongoose";
+import { NextResponse } from "next/server";
+import User from "@/models/User";
 
-export async function PUT(req,{ params }) {
-    try{
+export async function PUT(req, { params }) {
+    try {
         await connectDB();
-        const resolvedParams= await params;
-        const id = resolvedParams.id ;
-        const body= await req.json();
-        const updatedUser =await User.findByIdAndUpdate(
+        const resolvedParams = await params;
+        const id = resolvedParams.id;
+        const body = await req.json();
+        const updatedUser = await User.findByIdAndUpdate(
             id,
             body,
-            {new:true ,runValidators: true}
+            { new: true, runValidators: true }
         );
         return NextResponse.json({
-            message:"user updated successfully",
-            data:updatedUser})
-    }catch(err){
-            return NextResponse.json({
-                message:"Error creating user",
-                error:err.message
-            });
+            message: "user updated successfully",
+            data: updatedUser
+        })
+    } catch (err) {
+        return NextResponse.json({
+            message: "Error creating user",
+            error: err.message
+        });
+    }
+}
+//delete 
+export async function DELETE(req, { params }) {
+    try {
+        await connectDB();
+
+        const {id} = await params;
+        
+
+        const deleteUser = await User.findByIdAndDelete(id);
+
+        return NextResponse.json({
+            message: "user deleted successfully",
+            data: deleteUser        })
+    } catch (err) {
+        return NextResponse.json({
+            message: "Error deleting user",
+            error: err.message
+        });
     }
 }
