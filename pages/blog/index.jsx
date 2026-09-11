34be54fde315790f2d4a041c89fabf88c2ee81c1
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-11 09:07:16 -0700

    adding links in blog page

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,5 +1,14 @@
+import Link from "next/link"
+
 export default function Blog(){
     return(
+        <div>
         <h1>Blog Page</h1>
+        <ul>
+            <li><Link href="/blog/1">Blog1</Link></li>
+            <li><Link href="/blog/react"> React Blog</Link></li>
+            <li><Link href="/blog/next.js"> Next.js Blog</Link></li>
+        </ul>
+        </div>
     );
 }
