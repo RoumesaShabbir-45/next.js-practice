5e8bcc308e7cb1c64d995b4ff0b9ac7d06d3170b
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-12 06:38:04 -0700

    revaldation with SSG and ISR

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,51 +1,51 @@
-import { useEffect ,useState } from "react";
+// import { useEffect ,useState } from "react";
 
-export default function Home(){
-    const [user,setUsers]=useState([]);
+// export default function Home(){
+//     const [user,setUsers]=useState([]);
 
-    useEffect(() => {
-        async  function fetchData(){
-            const res= await fetch("https://jsonplaceholder.typicode.com/users");
-                const data = await res.json();
-                setUsers(data);
-        }
-        fetchData();
-    },[])
-    return(
-        <div>
-            <h1>User List(CSR)</h1>
-            {
-             user.map((user)=>(
-                    <p key={user.id}>{user.name}</p>
-                ))
-            }
-            </div>
-    );  
-}
-
-// export async function getStaticProps() {
-//     const res = await fetch("https://jsonplaceholder.typicode.com/users");
-//     const data = await res.json();
-//     return {
-//         props: {
-//             users: data,
-//         },
-//         revalidate: 10,
-//     };
-// }
-
-// export default function Home({ users }) {
-
-//     return (
+//     useEffect(() => {
+//         async  function fetchData(){
+//             const res= await fetch("https://jsonplaceholder.typicode.com/users");
+//                 const data = await res.json();
+//                 setUsers(data);
+//         }
+//         fetchData();
+//     },[])
+//     return(
 //         <div>
-//             <h1>User List(SSG+ISR)</h1>
+//             <h1>User List(CSR)</h1>
 //             {
-//                 users.map((user) => (
+//              user.map((user)=>(
 //                     <p key={user.id}>{user.name}</p>
 //                 ))
 //             }
-//         </div>
-
-//     );
+//             </div>
+//     );  
 // }
+
+export async function getStaticProps() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    return {
+        props: {
+            users: data,
+        },
+        revalidate: 10,
+    };
+}
+
+export default function Home({ users }) {
+
+    return (
+        <div>
+            <h1>User List(SSG+ISR)</h1>
+            {
+                users.map((user) => (
+                    <p key={user.id}>{user.name}</p>
+                ))
+            }
+        </div>
+
+    );
+}
 
